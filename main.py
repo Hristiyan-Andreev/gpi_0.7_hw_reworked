8354a392ed79942fc09638c0a9b3b827821c82c1
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-04-24 15:51:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,56 +8,56 @@ from s_av_ctrl import StreamAvailController as StreamAvailCtrl
 from reloader import Reloader
 from state_manager import StateManager
 from helpers import setup_main_logger
-
-main_log = setup_main_logger()
-main_log.info('Starting Ad Avail Controller ver. 0.7')
-
-
 # sys.path.append('/home/pi/config')
 import config as cf
 
-main_log.info('Configuration files imported')
 
-# Set-up state manager
-stater = StateManager(cf.LAST_EXIT_FILE, cf.STATE_FILE)
-
-# Make a new dict with GPIs as Keys and (class)StreamAvailCtrl as values
-# Check wether last exit was from reload and load state if necessary
-gpi_event_dict = {}
-if stater.is_last_exit_from_reload() is True:
-    gpi_cue_state = stater.load_gpi_state()
+def start_up_squence():
     
+    main_log = setup_main_logger()
+    main_log.info('Starting Ad Avail Controller ver. 0.7')
+    # main_log.info('Configuration files imported')
 
-    for gpi, id in cf.gpi2stream.items():
-        gpi_event_dict[gpi] = StreamAvailCtrl(gpi, id, cf.elemental_ip,\
-            in_cue = gpi_cue_state[gpi])
+    # Set-up state manager
+    stater = StateManager(cf.LAST_EXIT_FILE, cf.STATE_FILE)
 
-elif stater.is_last_exit_from_reload() is False:
-    for gpi, id in cf.gpi2stream.items():
-        gpi_event_dict[gpi] = StreamAvailCtrl(gpi, id, cf.elemental_ip)
+    # Make a new dict with GPIs as Keys and (class)StreamAvailCtrl as values
+    # Check wether last exit was from reload and load state if necessary
+    gpi_event_dict = {}
+    if stater.is_last_exit_from_reload() is True:
+        gpi_cue_state = stater.load_gpi_state()
+        
+
+        for gpi, id in cf.gpi2stream.items():
+            gpi_event_dict[gpi] = StreamAvailCtrl(gpi, id, cf.elemental_ip,\
+                in_cue = gpi_cue_state[gpi])
+
+    elif stater.is_last_exit_from_reload() is False:
+        for gpi, id in cf.gpi2stream.items():
+            gpi_event_dict[gpi] = StreamAvailCtrl(gpi, id, cf.elemental_ip)
 
 
-main_log.info('State loaded')
-main_log.info(gpi_event_dict['12'])
-# Set-up reloader on file changes
+    main_log.info('State loaded')
+    main_log.info(gpi_event_dict['12'])
+    # Set-up reloader on file changes
 
-reload_thread = Reloader(cf.WATCHED_FILES, linux=True,\
-     before_reload= stater.save_gpi_state, gpi_event_dict = gpi_event_dict)
-reload_thread.start()
-main_log.info('Reloading enabled')
+    reload_thread = Reloader(cf.WATCHED_FILES, linux=True,\
+        before_reload= stater.save_gpi_state, gpi_event_dict = gpi_event_dict)
+    reload_thread.start()
+    main_log.info('Reloading enabled')
 
-# Setup GPIO inputs/outputs
-    #Use Board pin numbering - etc. (12) in pinout command
-# GPIO.setmode(GPIO.BCM)
-#     #Setup GPIOs as inputs with PULL-UP
-# for GPI in list(cf.gpi2stream):
-#     GPIO.setup( GPI, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    # Setup GPIO inputs/outputs
+    #     Use Board pin numbering - etc. (12) in pinout command
+    GPIO.setmode(GPIO.BCM)
+    #Setup GPIOs as inputs with PULL-UP
+    for GPI in list(cf.gpi2stream):
+        GPIO.setup( GPI, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-# # Tie callbacks to events
+    # Tie callbacks to events
 
-# for GPI in list(cf.gpi2stream):
-#     GPIO.add_event_detect( GPI, GPIO.BOTH, callback = gpi_event_dict[GPI].\
-#         start_stop_avail , bouncetime = 20)
+    for GPI in list(cf.gpi2stream):
+        GPIO.add_event_detect( GPI, GPIO.BOTH, callback = gpi_event_dict[GPI].\
+            start_stop_avail , bouncetime = 20)
 
 
 if __name__ == '__main__':
