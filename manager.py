5bade965e2413ee9ec0f9a321e1c05a167daa41b
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-04-27 16:30:55 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -3,6 +3,11 @@ import subprocess as subp
 # import click as cl
 # import PyInquirer as pyq
 
+# TODO: Master script to 
+# 1) start/stop/restart the main.py
+# 2) enable/disable autostart
+# 3) Show output and logs
+# 4) monitor (health check) the main script
 
 inp = input("Start the bloody program!")
 process = subp.Popen(['python3','main.py'], stdout=subp.PIPE)
