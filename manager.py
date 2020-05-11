6feaf21b718615d08a79b5fd3deb6b973580c465
Hristiyan Andreev <hrvalandreev@gmail.com>
2020-05-11 12:45:41 +0300

    Find main.py PID

--- manager.py
+++ manager.py
@@ -9,6 +9,29 @@ import subprocess as subp
 # 3) Show output and logs
 # 4) monitor (health check) the main script
 
-inp = input("Start the bloody program!")
-process = subp.Popen(['python3','main.py'], stdout=subp.PIPE)
-print(process.communicate())
+# def check_status_main():
+#     this_pid = os.getpid()
+#     python_pids = get_pid('python3')
+def get_pid(name):
+    return list(map(int,subp.check_output(["pidof", "-c", name]).split()))
+
+
+#-- Test pgrep
+this_pid = os.getpid()
+print(this_pid)
+
+python_pids = get_pid('python3')
+main_py_pids = list(map(int,subp.check_output(["pgrep", "-f", "main.py"]).split()))
+print(python_pids)
+print(main_py_pids)
+
+python_main_py_pid = set(python_pids).intersection(main_py_pids)
+print(python_main_py_pid)
+
+while(True):
+    pass
+
+# -- Start the main program from this process -- 
+# inp = input("Start the bloody program!")
+# process = subp.Popen(['python3','main.py'], stdout=subp.PIPE)
+# print(process.communicate())
